a8dfeb955eaa8768b142613ae12f0ebc990ad2ee
Akhil1812007 <akhil.bindhu@outlook.com>
2022-08-25 11:14:02 +0530

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = { 3, 5, 1, 4, 9, 7, 21, 3, 7 };
+            InsertionSort(input, input.Length);
+            Console.WriteLine("the sorted array is");
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i] + " ");
+            }
+        }
+        public static void InsertionSort(int[] array, int size)
+        {
+            int key, j;
+            for (int i = 1; i < size; i++)
+            {
+                key = array[i];
+                j = i;
+                
+                while (j > 0 && array[j - 1] > key)
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = key;
+                
+            }
+        }
+
+    }
+}
